2285fbd742749946d9cbea3fcf77f26fa6780de2
franayerza <franayerza@gmail.com>
2020-06-19 23:22:15 -0300

    Merge onto master

--- routes/home.js
+++ routes/home.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const homeController = require("../controllers/homeController")
 
 router.get("/", homeController.root);
+router.get('/search', homeController.search);
 
 module.exports = router;
