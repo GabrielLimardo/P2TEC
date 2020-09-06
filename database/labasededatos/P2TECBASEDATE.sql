c2deafe56fdc1d2c77f084f2e5918b77a38538c6
Tobias Vasquez <tutivasquez@hotmail.com>
2020-09-05 21:10:00 -0300

    Agregadas nuevas brands y modificado el ruteo

--- database/labasededatos/P2TECBASEDATE.sql
+++ database/labasededatos/P2TECBASEDATE.sql
@@ -23,7 +23,7 @@ CREATE TABLE brands (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO brands VALUES (1,'Aorus','2020-07-15 11:34:38',NULL,NULL),(2,'Asus','2020-07-15 11:34:38',NULL,NULL),(3,'Adata','2020-07-15 11:34:38',NULL,NULL),(4,'Seagate','2020-07-15 11:34:38',NULL,NULL),(5,'AMD','2020-07-15 11:34:38',NULL,NULL),(6,'Ballistix','2020-07-15 11:34:38',NULL,NULL),(7,'HyperX','2020-09-04 19:01:38',NULL,NULL),(8,'Samsung','2020-09-04 19:02:38',NULL,NULL),(9,'AlienWare','2020-09-04 19:17:38',NULL,NULL),(10,'Corsair','2020-09-04 19:17:38',NULL,NULL),(11,'Logitech','2020-09-04 19:18:38',NULL,NULL),(12,'AOC','2020-09-04 19:18:38',NULL,NULL),(13,'Kingston','2020-09-04 19:18:38',NULL,NULL),(14,'CoolerMaster','2020-09-04 19:22:38',NULL,NULL),(15,'Intel','2020-09-04 19:22:38',NULL,NULL);
+INSERT INTO brands VALUES (1,'Aorus','2020-07-15 11:34:38',NULL,NULL),(2,'Asus','2020-07-15 11:34:38',NULL,NULL),(3,'Adata','2020-07-15 11:34:38',NULL,NULL),(4,'Seagate','2020-07-15 11:34:38',NULL,NULL),(5,'AMD','2020-07-15 11:34:38',NULL,NULL),(6,'Ballistix','2020-07-15 11:34:38',NULL,NULL),(7,'HyperX','2020-09-04 19:01:38',NULL,NULL),(8,'Samsung','2020-09-04 19:02:38',NULL,NULL),(9,'AlienWare','2020-09-04 19:17:38',NULL,NULL),(10,'Corsair','2020-09-04 19:17:38',NULL,NULL),(11,'Logitech','2020-09-04 19:18:38',NULL,NULL),(12,'AOC','2020-09-04 19:18:38',NULL,NULL),(13,'Kingston','2020-09-04 19:18:38',NULL,NULL),(14,'CoolerMaster','2020-09-04 19:22:38',NULL,NULL),(15,'Intel','2020-09-04 19:22:38',NULL,NULL),(16,'Tenda','2020-09-05 20:58:38',NULL,NULL),(17,'HP','2020-09-05 20:58:38',NULL,NULL),(18,'Marvo','2020-09-05 20:58:38',NULL,NULL),(19,'Trust','2020-09-05 20:58:38',NULL,NULL),(20,'Razer','2020-09-05 20:58:38',NULL,NULL);
 
 CREATE TABLE products (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -45,34 +45,34 @@ CREATE TABLE products (
 
 
 INSERT INTO products VALUES
-(1,'Disco Solido Ssd 480 GB',7471,1,3,'1 (5).jpg','Disco Solido Ssd 480 Gb Kingston A400','2020-07-15 11:34:38','2020-07-25 22:35:12','2020-07-25 22:35:12'),
-(2,'WEBCAM LOGITECH C920s HD PRO',24990,4,6,'webcam.jpg','Videoconferencias Full HD 1080p (hasta 1920 x 1080 píxeles)','2020-07-15 11:34:38',NULL,NULL),
-(3,'Fuente Cooler Master MWE 500w 80 Plus Bronze',7514,1,1,'1 (26).jpg','Disco Solido Ssd 480 Gb Kingston A400','2020-07-15 11:34:38',NULL,NULL),
-(4,'MEMORIA',8274,1,6,'1 (12).jpg','Memoria Ddr4 16Gb 2400Mhz CL17 (KVR24N17D8/16) Kingston','2020-07-15 11:34:38',NULL,NULL),
-(5,'Monitor Led AOC 15.6B',7504,3,2,'1 (37).jpg','Monitor Led AOC 15.6 Wide E1670SWU-E USB (incluye imp interno)','2020-07-15 11:34:38',NULL,NULL),
-(6,'Monitor Samsung 22',15094,3,2,'1 (38).jpg','Monitor Samsung 22 Full HD S22F350FH - c/Hdmi','2020-07-15 11:34:38',NULL,NULL),
-(7,'AURICULARES HYPERX CLOUD STINGER CORE',6939,4,4,'hyperx.jpg','Altavoces: Dinámico, Ø 40mm, con magnetos de neodimio','2020-07-15 11:34:38',NULL,NULL),
-(8,'AURICULARES HYPERX CLOUD CHAT PS4',1944,4,4,'aurips4.jpg','HyperX Cloud Chat™ Headset con licencia PS4 están diseñados para una comunicación nítida para chat. ','2020-07-15 11:34:38',NULL,NULL),
-(9,'AURICULARES ASUS ROG DELTA CORE',19770,4,4,'rodge.jpg','Marca ASUS Modelo 90YH00Z1-B2UA00 Nombre del modelo ROG Delta Año de fabricación 2018 Peso del producto','2020-07-15 11:34:38',NULL,NULL),
-(10,'TECLADO GAMER',15357,4,3,'1 (45).jpg','Teclado Gamer Corsair K70 RGB MK.2 Mechanical Gaming CHERRY® MX Blue','2020-07-15 11:34:38',NULL,NULL),
-(11,'Notebook Alienware I7',240000,2,3,'alien.jpg','17r5 Intel Core I7-8750h X6 2.2ghz 16gb 1.1tb 17','2020-07-15 11:34:38',NULL,NULL),
-(12,'Monitor Gamer Curvo 34',15094,3,3,'monitorcurvo.jpg','Monitor Ultrawide Ips Gsync Wfhd 144hz 1ms H','2020-07-15 11:34:38',NULL,NULL),
-(13,'PC Home Office',40261,6,3,'PC-Oficina-2_800.jpg','Intel Core I3 8100 - H310 - 8GB - 240GB SS','2020-07-15 11:34:38',NULL,NULL),
-(14,'PC Home Office',39660,6,3,'PC-Oficina-2_800.jpg','Intel Core I3 9100F - H310 - 8GB - GT 210 - 240GB SS','2020-07-15 11:34:38',NULL,NULL),
-(15,'PC Home Office',32343,6,3,'PC-Oficina-2_800.jpg','Intel Pentium G5400 - H310 - 8GB - 240GB SSD','2020-07-15 11:34:38',NULL,NULL),
-(16,'ACCESS POINT',6954,4,3,'1 (18).jpg','Sistema Wi-Fi Mesh Tenda Nova MW3 2-pack AC1200','2020-07-15 11:34:38',NULL,NULL),
-(17,'PC Streaming',82768,5,3,'PCstremingultima.jpg','Intel I5 9400 - B365 - 8GB - GTX 1650 S - 1TB','2020-07-15 11:34:38',NULL,NULL),
-(18,'PC Streaming',108012,5,1,'PCstremingultima.jpg','Intel I5 9400 - B365 - 16GB - GTX 1660 Ti - 240GB SSD - 1TB','2020-07-15 11:34:38',NULL,NULL),
-(19,'PC Streaming',110554,5,1,'PCstremingultima.jpg','AMD Ryzen 5 3600 - B450 - 16GB - RX 5600 XT - 240GB SSD - 1T','2020-07-15 11:34:38',NULL,NULL),
-(20,'Notebook Hp I5',111096,2,2,'1 (23).jpg','Notebook Hp I5 250 G7 8265U 8GB 1TB 15.6','2020-07-15 11:34:38',NULL,NULL),
-(21,'Notebook Hp I7',163224,2,2,'1 (40).jpg','Notebook Hp I7 10510U 8Gb !tb Windows 10 15,6 + GeForece MX 130','2020-07-15 11:34:38',NULL,NULL),
-(22,'JOYSTICK MARVO GT64 WIRELESS PS4 PC',6999,4,2,'marvojostick.jpg','GT-64 se recomienda para los entusiastas de los juegos de plataformas','2020-08-22 13:03:00',NULL,NULL),
-(23,'MOUSE TRUST KUDOS RGB GXT900',7373,4,4,'trustemouse.jpg','Gaming Designed for gaming yes Game type FPS, MOBA, RTS, RPG','2020-08-22 13:03:00',NULL,NULL),
-(24,'MOUSE GAMER RAZER VIPER AMBIDEXTROUS WIRED',9425,4,4,'razermopuse.jpg','Hasta 450 pulgadas por segundo (IPS)/50 G de aceleración','2020-08-22 13:03:00',NULL,NULL),
-(25,'MOUSE TRUST HERON RGB GXT 170',4560,4,4,'trustehero.jpg','Ratón para jugar, con LED RGB y sensor óptico avanzado.','2020-08-22 13:04:00',NULL,NULL),
-(26,'PC Diseño',82768,7,5,'pcdiseno.jpg','Intel I5 9400 - B365 - 8GB - GTX 1650 S - 1TB','2020-07-15 11:34:38',NULL,NULL),
-(27,'PC Diseño',108012,7,6,'pcdiseno.jpg','Intel I5 9400 - B365 - 16GB - GTX 1660 Ti - 240GB SSD - 1TB','2020-07-15 11:34:38',NULL,NULL),
-(28,'PC Diseño',110554,7,6,'pcdiseno.jpg','AMD Ryzen 5 3600 - B450 - 16GB - RX 5600 XT - 240GB SSD - 1T','2020-07-15 11:34:38',NULL,NULL);
+(1,'Disco Solido Ssd 480 GB',7471,1,13,'1 (5).jpg','Disco Solido Ssd 480 Gb Kingston A400','2020-07-15 11:34:38','2020-07-25 22:35:12','2020-07-25 22:35:12'),
+(2,'WEBCAM LOGITECH C920s HD PRO',24990,4,11,'webcam.jpg','Videoconferencias Full HD 1080p (hasta 1920 x 1080 píxeles)','2020-07-15 11:34:38',NULL,NULL),
+(3,'Fuente Cooler Master MWE 500w 80 Plus Bronze',7514,1,14,'1 (26).jpg','Disco Solido Ssd 480 Gb Kingston A400','2020-07-15 11:34:38',NULL,NULL),
+(4,'Memoria Kingston',8274,1,13,'1 (12).jpg','Memoria Ddr4 16Gb 2400Mhz CL17 (KVR24N17D8/16) ','2020-07-15 11:34:38',NULL,NULL),
+(5,'Monitor Led AOC 15.6B',7504,3,12,'1 (37).jpg','Monitor Led AOC 15.6 Wide E1670SWU-E USB (incluye imp interno)','2020-07-15 11:34:38',NULL,NULL),
+(6,'Monitor Samsung 22',15094,3,8,'1 (38).jpg','Monitor Samsung 22 Full HD S22F350FH - c/Hdmi','2020-07-15 11:34:38',NULL,NULL),
+(7,'AURICULARES HYPERX CLOUD STINGER CORE',6939,4,7,'hyperx.jpg','Altavoces: Dinámico, Ø 40mm, con magnetos de neodimio','2020-07-15 11:34:38',NULL,NULL),
+(8,'AURICULARES HYPERX CLOUD CHAT PS4',1944,4,7,'aurips4.jpg','HyperX Cloud Chat™ Headset con licencia PS4 están diseñados para una comunicación nítida para chat. ','2020-07-15 11:34:38',NULL,NULL),
+(9,'AURICULARES ASUS ROG DELTA CORE',19770,4,2,'rodge.jpg','Marca ASUS Modelo 90YH00Z1-B2UA00 Nombre del modelo ROG Delta Año de fabricación 2018 Peso del producto','2020-07-15 11:34:38',NULL,NULL),
+(10,'TECLADO GAMER Corsair',15357,4,10,'1 (45).jpg','Teclado Gamer Corsair K70 RGB MK.2 Mechanical Gaming CHERRY® MX Blue','2020-07-15 11:34:38',NULL,NULL),
+(11,'Notebook Alienware I7',240000,2,9,'alien.jpg','17r5 Intel Core I7-8750h X6 2.2ghz 16gb 1.1tb 17','2020-07-15 11:34:38',NULL,NULL),
+(12,'Monitor Gamer Curvo 34',15094,3,8,'monitorcurvo.jpg','Monitor Ultrawide Ips Gsync Wfhd 144hz 1ms H','2020-07-15 11:34:38',NULL,NULL),
+(13,'PC Home Office',40261,6,15,'PC-Oficina-2_800.jpg','Intel Core I3 8100 - H310 - 8GB - 240GB SS','2020-07-15 11:34:38',NULL,NULL),
+(14,'PC Home Office',39660,6,15,'PC-Oficina-2_800.jpg','Intel Core I3 9100F - H310 - 8GB - GT 210 - 240GB SS','2020-07-15 11:34:38',NULL,NULL),
+(15,'PC Home Office',32343,6,15,'PC-Oficina-2_800.jpg','Intel Pentium G5400 - H310 - 8GB - 240GB SSD','2020-07-15 11:34:38',NULL,NULL),
+(16,'ACCESS POINT',6954,4,16,'1 (18).jpg','Sistema Wi-Fi Mesh Tenda Nova MW3 2-pack AC1200','2020-07-15 11:34:38',NULL,NULL),
+(17,'PC Streaming',82768,5,15,'PCstremingultima.jpg','Intel I5 9400 - B365 - 8GB - GTX 1650 S - 1TB','2020-07-15 11:34:38',NULL,NULL),
+(18,'PC Streaming',108012,5,15,'PCstremingultima.jpg','Intel I5 9400 - B365 - 16GB - GTX 1660 Ti - 240GB SSD - 1TB','2020-07-15 11:34:38',NULL,NULL),
+(19,'PC Streaming',110554,5,5,'PCstremingultima.jpg','AMD Ryzen 5 3600 - B450 - 16GB - RX 5600 XT - 240GB SSD - 1T','2020-07-15 11:34:38',NULL,NULL),
+(20,'Notebook Hp I5',111096,2,17,'1 (23).jpg','Notebook Hp I5 250 G7 8265U 8GB 1TB 15.6','2020-07-15 11:34:38',NULL,NULL),
+(21,'Notebook Hp I7',163224,2,17,'1 (40).jpg','Notebook Hp I7 10510U 8Gb !tb Windows 10 15,6 + GeForece MX 130','2020-07-15 11:34:38',NULL,NULL),
+(22,'JOYSTICK MARVO GT64 WIRELESS PS4 PC',6999,4,18,'marvojostick.jpg','GT-64 se recomienda para los entusiastas de los juegos de plataformas','2020-08-22 13:03:00',NULL,NULL),
+(23,'MOUSE TRUST KUDOS RGB GXT900',7373,4,19,'trustemouse.jpg','Gaming Designed for gaming yes Game type FPS, MOBA, RTS, RPG','2020-08-22 13:03:00',NULL,NULL),
+(24,'MOUSE GAMER RAZER VIPER AMBIDEXTROUS WIRED',9425,4,20,'razermopuse.jpg','Hasta 450 pulgadas por segundo (IPS)/50 G de aceleración','2020-08-22 13:03:00',NULL,NULL),
+(25,'MOUSE TRUST HERON RGB GXT 170',4560,4,19,'trustehero.jpg','Ratón para jugar, con LED RGB y sensor óptico avanzado.','2020-08-22 13:04:00',NULL,NULL),
+(26,'PC Diseño',82768,7,15,'pcdiseno.jpg','Intel I5 9400 - B365 - 8GB - GTX 1650 S - 1TB','2020-07-15 11:34:38',NULL,NULL),
+(27,'PC Diseño',108012,7,15,'pcdiseno.jpg','Intel I5 9400 - B365 - 16GB - GTX 1660 Ti - 240GB SSD - 1TB','2020-07-15 11:34:38',NULL,NULL),
+(28,'PC Diseño',110554,7,5,'pcdiseno.jpg','AMD Ryzen 5 3600 - B450 - 16GB - RX 5600 XT - 240GB SSD - 1T','2020-07-15 11:34:38',NULL,NULL);
 CREATE TABLE roles (
   rol_id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(45) DEFAULT NULL,
